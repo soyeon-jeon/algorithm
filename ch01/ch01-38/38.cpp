4e331d53780894fe9984e646d81bade93d8b3c3c
soyeon-jeon <wjsthdus45@cu.ac.kr>
2019-09-16 14:35:08 +0900

    0916

---
+++ ch01/ch01-38/38.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j;
+	int n;
+
+	printf("사각형을 출력합니다.\n"
+}
---
+++ ch01/ch01/44.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define N 5
+int main(void)
+{
+	int i;
+	int a[N];
+	for (i = 0; i < N; i++) {
+		printf("a[%d] : ", i);
+		scanf_s("5d", &a[i]);
+	}
+	puts("각 요소의 값");
+	for (i = 0; i < N; i++) {
+		printf("a[%d] = %d\n", i, a[i]);
+	}
+	return 0;
+}
