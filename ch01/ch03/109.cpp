2de9ffb552500a7a0c0d377cc34221c83f60e53d
soyeon-jeon <wjsthdus45@cu.ac.kr>
2019-09-27 11:25:38 +0900

    0927

--- ch01/ch03/109.cpp
+++ ch01/ch03/109.cpp
@@ -26,19 +26,19 @@ int main(void)
 	int*x;
 	puts("이진검색");
 	printf("요소 개수 : ");
-	scanf("%d" , &nx);
+	scanf_s("%d" , &nx);
 	x = (int*)calloc(nx, sizeof(int));
 	printf("오름차순으로 입력하세요.\n");
 	printf("x[0] : ");
 	for (i = 1; i < nx; i++) {
 		do {
 			printf("x[%d] : ", i);
-			scanf("%d", &x[i]);
+			scanf_s("%d", &x[i]);
 
 		} while (x[i] < x[i - 1]);
 	}
 	printf("검색값 : ");
-	scanf("%d", &ky);
+	scanf_s("%d", &ky);
 	idx = bin_search(x, nx, ky);
 	if (idx == -1)
 		puts("검색에 실패했습니다.");
